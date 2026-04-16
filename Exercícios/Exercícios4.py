f9d526217f3d0745ac31dcc6e52fb7207417bd4e
Vinícius Mansur <vinim1403@gmail.com>
2026-04-15 22:45:50 -0300

    Feat: Exercícios 5 ao 8

--- Exercícios/Exercícios4.py
+++ Exercícios/Exercícios4.py
@@ -27,19 +27,61 @@ if flag:
 
 # Exercício 2
 num = 1
+
 while num <= 100:
     print(num)
     num += 1
 
 # Exercício 3
 num = 50
+
 while num <= 100:
     print(num)
     num += 1
 
 # Exercício 4
 num = 10
+
 while num >= 0:
     print(num)
     num -= 1
 print('Fogo!')
+
+# Exercício 5
+num = int(input("Digite um número: "))
+numver = 1
+
+while numver <= num:
+    print(numver)
+    numver += 1
+    if numver % 2 == 1:
+        numver += 1
+
+# Exercício 6
+num = int(input("Digite um número: "))
+numver = 1
+
+while numver <= num:
+    print(numver)
+    numver += 2
+
+# Exercício 7
+comeco = int(input("Digite um número: "))
+fim = int(input("Digite outro número maior que o outro: "))
+soma = 0
+
+
+while comeco <= fim:
+    if comeco % 2 == 0:
+        soma = comeco + soma
+    comeco += 1
+
+print(f'A soma dos números pares desse intervalo é {soma}')
+
+# Exercício 8
+num = int(input("Digite um número: "))
+soma = 0
+while num != 0:
+    soma = num + soma
+    num = int(input("Digite um número: "))
+print(f"você digitou 0! A soma dos números digitados foram: {soma}")
