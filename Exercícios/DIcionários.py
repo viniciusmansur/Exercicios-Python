9e531a8b96c5be077bec5f434b8fb967acf4a28c
Vinícius Mansur <vinim1403@gmail.com>
2026-07-02 03:15:49 -0300

    feat: add dicionários

---
+++ Exercícios/DIcionários.py
@@ -0,0 +1,7 @@
+capitais = {"Brasil": "Brasília",
+            "EUA": "Washington D.C.",
+            "França": "Paris",
+            "Alemanha": "Berlim"}
+
+print(help(capitais))
+print(capitais.items())
